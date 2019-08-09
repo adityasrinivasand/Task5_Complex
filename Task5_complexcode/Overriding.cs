06e4f8297ead2705e6d386f711aba7eb3669b714
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-09 16:45:45 +0530

    Create Overriding.cs

---
+++ Task5_complexcode/Overriding.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_complexcode
+{
+    class Shape
+    {
+        public virtual void Draw(int a)
+        {
+            Console.WriteLine($" Draw Function has been invoked with a parametric length: {a} ");
+        }
+    }
+    class Square : Shape
+    {
+        public override void Draw(int a)
+        {
+            Console.WriteLine($"Square Drawn with a length: {a} ");
+            Console.WriteLine($"Area of Square Drawn with a length: {a} is {a*a} ");
+        }
+
+    }
+    class Circle : Shape
+    {
+        public override void Draw(int a)
+        {
+            Console.WriteLine($"Circle Drawn with a Radius: {a} ");
+            Console.WriteLine($"Area of Circle Drawn with a radius: {a} is {3.14 * a * a} ");
+
+        }
+
+    }
+}
