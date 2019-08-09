bb888f7adaf8c711d10024691c87c3c00b10f820
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-09 16:45:12 +0530

    Create Constructor.cs

---
+++ Task5_complexcode/Constructor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_complexcode
+{
+    class Construct
+    {
+        public string p1, p2;
+        public int p3, p4;
+        public Construct(string x, string y)
+        {
+            p1 = x;
+            p2 = y;
+        }
+        public Construct(Construct gf)
+        {
+            p1 = gf.p1;
+            p2 = gf.p2;
+        }
+        public Construct(int a, int b)
+        {
+            p3 = a;
+            p4 = b;
+        }
+        public Construct(Construct a, Construct b)
+        {
+            p3 = a.p3;
+            p4 = b.p4;
+        }
+        ~Construct()
+        {
+            Console.WriteLine("Destructor is called");
+        }
+    }
+}
