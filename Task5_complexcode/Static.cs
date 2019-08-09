599d40e5d941b6d4bdf4641697533aaf90ff4f52
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-09 18:41:32 +0530

    Create Static.cs

---
+++ Task5_complexcode/Static.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_Complexcode
+{
+    static class User
+    {
+        public static string name;
+        public static string location;
+        public static int age;
+        public static void add()
+        {
+            Console.Write("Enter Employee Name:");
+            User.name = Console.ReadLine();
+            Console.Write("Enter Employee Location:");
+            User.location = Console.ReadLine();
+            Console.Write("Enter Employee age:");
+            User.age = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("USer Detail Added Successfully...!!!!:");
+        }
+        public static void display()
+        {
+            Console.WriteLine("Name: {0}", User.name);
+            Console.WriteLine("Location: {0}", User.location);
+            Console.WriteLine("Age: {0}", User.age);
+        }
+    }
+}
