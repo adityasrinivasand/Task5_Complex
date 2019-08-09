ebf1bdee62efe7250f2edcd4687692c50b539cb8
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-09 18:41:30 +0530

    Update Program.cs

--- Task5_complexcode/Program.cs
+++ Task5_complexcode/Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task5_complexcode
+namespace Task5_Complexcode
 {
     class Program
     {
         static void Main(string[] args)
         {
-                                        /* Overloading, Overiding, polymorphism , Inheritance */
+                                        /* Overloading, Overiding, polymorphism , Inheritance, Type Conversion */
 
             Console.WriteLine("\t\t\t C# Complex");
             Console.WriteLine("");
@@ -18,13 +18,12 @@ namespace Task5_complexcode
             Shape[] s = new Shape[2];
             s[0] = new Circle();
             s[1] = new Square();
-            Console.WriteLine("Enter a number");
-            var number = Console.ReadLine();
-            int num;
-            while (!int.TryParse(number, out num))
+            Console.WriteLine("Enter a Decimal number");
+
+            float num;
+            while (!float.TryParse(Console.ReadLine(), out num)) //checking whether it is float
             {
-                Console.WriteLine("This is not a number!");
-                number = Console.ReadLine();
+                Console.WriteLine("This is not a Decimal Number!");
             }
             Console.WriteLine("");
             for (int i = 0; i < 2; i++)
@@ -72,9 +71,12 @@ namespace Task5_complexcode
             ComplexNum cnum2 = new ComplexNum(c3, c3);
 
             ComplexNum addition = cnum1 + cnum2;
+            ComplexNum subtraction = cnum1 - cnum2;
             Console.WriteLine("First complex number:  {0}", cnum1);
             Console.WriteLine("Second complex number: {0}", cnum2);
-            Console.WriteLine("RESULT :The sum of the two numbers: {0}", addition);
+            Console.WriteLine("RESULT :The Sum of the two numbers: {0}", addition);
+            Console.WriteLine();
+            Console.WriteLine("RESULT :The Difference of the two numbers: {0}", subtraction);
             Console.ReadLine();
 
                                         /* Looping with conditional */
@@ -95,6 +97,7 @@ namespace Task5_complexcode
             Console.ReadLine();
 
                                         /* Exceptional Handling */
+
             Console.WriteLine("\t\t Exceptional Handling Complex O/P");
             Console.WriteLine("Enter the Temperature in Celsius");
             var temperature = Convert.ToDouble(Console.ReadLine());
@@ -108,6 +111,38 @@ namespace Task5_complexcode
                 Console.WriteLine("TempIsZeroException: {0}", e.Message);
             }
             Console.ReadLine();
+
+                                        /* Static with Conversion */
+            Console.WriteLine("\t\t Static Complex O/P");
+            char ans;
+            do
+            {
+                Console.WriteLine("\t\t Welcome");
+                Console.WriteLine("");
+                Console.WriteLine("1. Add  an User");
+                Console.WriteLine("2. View User details");
+                Console.WriteLine("");
+                Console.Write("Enter Your Choice Here:-");
+                int choose_number = Convert.ToInt32(Console.ReadLine());
+
+                switch (choose_number)
+                {
+                    case 1:
+                        User.add();
+                        Console.WriteLine("");
+                        break;
+                    case 2:
+                        User.display();
+                        Console.WriteLine("");
+                        break;
+                }
+                Console.WriteLine("");
+                Console.Write("Would You Like To Continue(Y/N):");
+                ans = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("");
+            } while (ans == 'y' || ans == 'Y');
+            Console.ReadLine();
+
         }
     }
 }
